84c89e0928ad384b6fec2a17d44ae4f4f2a04422
klickverbot <klickverbot@gmail.com>
2009-06-20 17:03:10 +0200

    Refactored generic parts of the View out into FreeCameraApplication.

--- src/Viewer.d
+++ src/Viewer.d
@@ -25,7 +25,7 @@ import d4.scene.Node;
 import d4.scene.Scene;
 import d4.scene.Vertex;
 import d4.util.Key;
-import d4.util.SdlApplication;
+import FreeCameraApplication;
 
 /**
  * The available shading modes.
@@ -40,7 +40,7 @@ enum ShadingMode {
  * The main application class.
  * Manages the scene, reacts to user input, etc.
  */
-class Viewer : SdlApplication {
+class Viewer : FreeCameraApplication {
 public:
    this( char[][] args ) {
       // Parse command line options.
@@ -67,13 +67,6 @@ protected:
       Stdout.newline;
       m_scene = new AssimpScene( m_sceneFileName, m_generateSmoothNormals, m_fakeColors );
 
-      m_renderer = new Renderer( screen() );
-      m_renderer.backfaceCulling = BackfaceCulling.CULL_CW;
-      m_renderer.setProjection( PI / 3, 0.5f, 1000f );
-
-      m_cameraPosition = Vector3( 0, 0, 10 );
-      m_cameraRotation = Quaternion();
-
       m_materialManager = new MaterialManager( m_renderer );
       // Enable everything by default.
       m_shadingMode = ShadingMode.GOURAUD_TEXTURED;
@@ -86,14 +79,14 @@ protected:
    }
 
    override void render( float deltaTime ) {
+      super.render( deltaTime );
+
       if ( m_animateBackground ) {
          updateRainbowBackground( deltaTime );
       }
       if ( m_rotateWorld ) {
          updateRotatingWorld( deltaTime );
       }
-
-      updateCamera( deltaTime );
 
       m_renderer.beginScene();
       m_scene.rootNode.render( m_renderer, m_materialManager );
@@ -148,48 +141,6 @@ private:
       m_scene.rootNode.transformation = rotation * m_scene.rootNode.transformation;
    }
 
-   void updateCamera( float deltaTime ) {
-      // Compute camera movement from keyboard input.
-      float movementSpeed = 6f;
-      float rotationSpeed = PI / 8;
-      if ( isKeyDown( Key.LSHIFT ) || isKeyDown( Key.RSHIFT ) ) {
-         movementSpeed *= 4;
-         rotationSpeed *= 3;
-      }
-
-      if ( isKeyDown( Key.UP ) ) {
-         m_cameraRotation.append( rotationQuaternion( -rotationSpeed * deltaTime, Vector3( 1, 0, 0 ) ) );
-      }
-      if ( isKeyDown( Key.DOWN ) ) {
-         m_cameraRotation.append( rotationQuaternion( rotationSpeed * deltaTime, Vector3( 1, 0, 0 ) ) );
-      }
-      if ( isKeyDown( Key.LEFT ) ) {
-         m_cameraRotation.append( rotationQuaternion( -rotationSpeed * deltaTime, Vector3( 0, 1, 0 ) ) );
-      }
-      if ( isKeyDown( Key.RIGHT ) ) {
-         m_cameraRotation.append( rotationQuaternion( rotationSpeed * deltaTime, Vector3( 0, 1, 0 ) ) );
-      }
-
-      Matrix4 invMat = m_renderer.viewMatrix.inversed();
-      Vector3 forwardDirection = -Vector3( invMat.m13, invMat.m23, invMat.m33 );
-      Vector3 leftDirection = -Vector3( invMat.m11, invMat.m21, invMat.m31 );
-
-      if ( isKeyDown( Key.w ) ) {
-         m_cameraPosition += forwardDirection * deltaTime * movementSpeed;
-      }
-      if ( isKeyDown( Key.s ) ) {
-         m_cameraPosition -= forwardDirection * deltaTime * movementSpeed;
-      }
-      if ( isKeyDown( Key.a ) ) {
-         m_cameraPosition += leftDirection * deltaTime * movementSpeed;
-      }
-      if ( isKeyDown( Key.d ) ) {
-         m_cameraPosition -= leftDirection * deltaTime * movementSpeed;
-      }
-
-      m_renderer.viewMatrix = rotationMatrix( m_cameraRotation ) * translationMatrix( -m_cameraPosition );
-   }
-
    void updateShadingMode() {
       switch ( m_shadingMode ) {
          case ShadingMode.FLAT:
@@ -211,16 +162,12 @@ private:
    bool m_generateSmoothNormals;
    bool m_fakeColors;
 
-   Renderer m_renderer;
    MaterialManager m_materialManager;
    Scene m_scene;
 
    bool m_rotateWorld;
    bool m_animateBackground;
    float m_backgroundTime;
-
-   Vector3 m_cameraPosition;
-   Quaternion m_cameraRotation;
 
    ShadingMode m_shadingMode;
 }
