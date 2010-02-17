b533ad1921f11752c1fe5b298d1a0ec218db93f1
klickverbot <klickverbot@gmail.com>
2010-02-17 17:04:21 +0100

    Added license information (GNU GPLv3).

--- src/d4/math/Vector4.d
+++ src/d4/math/Vector4.d
@@ -1,7 +1,20 @@
-module d4.math.Vector4;
+/*
+ * Copyright © 2010, klickverbot <klickverbot@gmail.com>.
+ *
+ * This file is part of d4, which is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * d4 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * d4. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import tango.io.Stdout;
-import d4.math.Vector3;
+module d4.math.Vector4;
 
 /**
  * A vector in three-dimensional space with homogeneous coordinates x, y, z, w.
